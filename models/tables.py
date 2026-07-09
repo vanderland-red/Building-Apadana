810efc386ae3e2237c35ae48391fa92592448f53
Mohammadreza <mohammad.za9297@gmail.com>
2026-07-09 21:37:23 +0330

    tables database is added

---
+++ models/tables.py
@@ -0,0 +1,81 @@
+from extentions import SQLAlchemy
+from datetime import datetime
+from extentions import db
+
+
+
+
+class User(db.Model):
+    __tablename__ = "user"
+
+    id = db.Column(db.Integer, primary_key=True)
+    full_name = db.Column(db.String(100), nullable=False)
+    phone = db.Column(db.String(20), unique=True, nullable=False)
+    email = db.Column(db.String(255), unique=True, nullable=False)
+    password = db.Column(db.String(255), nullable=False)
+    role = db.Column(db.String(20), nullable=False, default="user")
+    created_at = db.Column(db.DateTime, default=datetime.utcnow)
+
+    def __repr__(self):
+        return f"<User {self.full_name}>"
+    
+
+
+
+class Service(db.Model):
+    __tablename__ = "service"
+
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(100), nullable=False)
+    description = db.Column(db.Text)
+    icon = db.Column(db.String(255))
+    active = db.Column(db.Boolean, default=True, nullable=False)
+
+
+
+
+
+class ServiceRequest(db.Model):
+    __tablename__ = "service_request"
+
+    id = db.Column(db.Integer, primary_key=True)
+
+    user_id = db.Column(
+        db.Integer,
+        db.ForeignKey("user.id"),
+        nullable=False
+    )
+
+    service_id = db.Column(
+        db.Integer,
+        db.ForeignKey("service.id"),
+        nullable=False
+    )
+
+    address = db.Column(db.String(255), nullable=False)
+    description = db.Column(db.Text)
+    status = db.Column(db.String(20), default="pending", nullable=False)
+    created_at = db.Column(db.DateTime, default=datetime.utcnow)
+
+    user = db.relationship("User", backref="user_service_requests")
+    service = db.relationship("Service", backref="service_requests")
+
+
+
+
+
+class RequestImage(db.Model):
+    __tablename__ = "request_image"
+
+    id = db.Column(db.Integer, primary_key=True)
+
+    request_id = db.Column(
+        db.Integer,
+        db.ForeignKey("service_request.id"),
+        nullable=False
+    )
+
+    image_path = db.Column(db.String(255), nullable=False)
+
+    service_request = db.relationship("ServiceRequest", backref="request_images")
+
