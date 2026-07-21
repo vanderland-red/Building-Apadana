62fd40be67c3918fa38a5c16447f3a4f3c8caaac
Mohammadreza <mohammad.za9297@gmail.com>
2026-07-21 21:39:38 +0330

    description and remove btn click is adedd in admin request append page

--- blueprints/admin.py
+++ blueprints/admin.py
@@ -141,3 +141,17 @@ def select_request (id):
         return redirect(url_for("admin.request_users"))
 
     return redirect(url_for("admin.request_users"))
+
+@bp.route("/user/dashboard/delete-service/<int:id>", methods=["POST"])
+def delete_service_this(id):
+    
+    if request.method == "POST" :
+        item = ServiceRequest.query.filter(
+            ServiceRequest.id == id,
+        ).first_or_404()
+        
+        db.session.delete(item)
+        db.session.commit()
+
+        flash("سرویس حذف شد", "success")
+        return redirect(url_for("admin.request_users_append"))
